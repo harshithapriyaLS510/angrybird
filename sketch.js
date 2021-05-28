e545a3fb1ed23a7782b5d8744967bc62a0434162
harshithapriyals510 <harshita.hp.510@gmail.com>
2021-05-28 19:09:10 +0530

    mesage

--- sketch.js
+++ sketch.js
@@ -37,8 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6=new Log(230,180,80,PI/2)
-  chain= new Chain (bird.body,log6.body)
+    
+    sling= new Slingshot (bird.body,{x:200,y:100})
 }
 
 function draw(){
@@ -65,6 +65,13 @@ function draw(){
     bird.display();
     platform1.display();
 
-    log6.display();
-    chain.display();
+    
+    sling.display();
 }
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+ function mouseReleased(){
+     sling.fly()
+ }
